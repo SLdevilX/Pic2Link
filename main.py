9518048b162bbdf65553e99b1110b3d8625f3452
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-05-16 01:24:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ async def start(client, message):
     await message.reply_text(
         text=f"Hello {message.from_user.first_name},\nIm telegram to telegra.ph image uploader bot by @W4RR10R",
         disable_web_page_preview=True.
-         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Channel]", url=f"https://t.me/danumabots"), InlineKeyboardButton(text="Owner", url=f"https://t.me/Lasi99X")]])
+         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Channel]", url=f"https://t.me/danumabots"), [InlineKeyboardButton(text="Owner", url=f"https://t.me/Lasi99X")]])
     )
 
 
