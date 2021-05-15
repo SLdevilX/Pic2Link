2774d589aff1a12c5a5a48ba0c362e194d7d7399
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-05-16 01:10:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ TGraph = Client(
 @TGraph.on_message(filters.command("start"))
 async def start(client, message):
     await message.reply_text(
-        text=f"Hello {message.from_user.first_name},\nIm telegram to telegra.ph image uploader bot by Danuma Project [Channel](buttonurl=https://t.me/danumabots)[Owner](buttonurl=https://t.me/Lasi99X) ",
-
-     disable_web_page_preview=True
+        text=f"Hello {message.from_user.first_name},\nIm telegram to telegra.ph image uploader bot by @W4RR10R",
+        disable_web_page_preview=True
+    )
 
 
 @TGraph.on_message(filters.photo)
@@ -32,9 +32,9 @@ async def getimage(client, message):
     if not os.path.isdir(tmp):
         os.makedirs(tmp)
     img_path = os.path.join(tmp, str(uuid.uuid4()) + ".jpg")
-    dwn = await message.reply_text("Downloading...🚀", True)
+    dwn = await message.reply_text("Downloading to my server...", True)
     img_path = await client.download_media(message=message, file_name=img_path)
-    await dwn.edit_text("generating your Link...🚀")
+    await dwn.edit_text("Uploading as telegra.ph link...")
     try:
         response = upload_file(img_path)
     except Exception as error:
